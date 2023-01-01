3ab41ddc6edc84fa0a4f6b19353092eec2d7a2f8
Peter Arsenault <prarsena@gmail.com>
2023-01-01 10:16:33 -0500

    ts file

---
+++ app.ts
@@ -0,0 +1,27 @@
+import express from "npm:express@4.18.2";
+
+const app: express = express();
+
+app.get("/", (req: any, res: any) => {
+  res.send("Welcome to the Dinosaur API!");
+});
+
+app.get('*', (req: any, res: any) => {
+    const path = req.path.split('/');
+    console.log(path);
+    if (path.length > 2){
+      res.send(`Hello,${path.join(' ')}!`); 
+    } else {
+      console.log("Path is " + path[1])
+      if (path[1] == ''){
+        res.send(`Hello, World!`);  
+      } else {
+        res.send(`Hello, ${path[1]}!`);
+      }
+    }
+  })
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}` )
+});
